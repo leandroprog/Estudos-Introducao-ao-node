2cbf34b66fe0ffcf237da8ab5c26e93f198c2e4c
Leandro Rocha <prog.leandro@gmail.com>
2020-02-25 16:07:55 -0300

    Criando o CRUD

--- index.js
+++ index.js
@@ -2,8 +2,55 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.status(200).json({sucesso: true})
+server.use(express.json());
+
+const users = ['Leandro', 'Izabela', 'Ilma'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index',(req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+
+server.post('/users', (req, res) =>  {
+  const { name } = req.body;
+
+  users.push(name)
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  
+  const { index } = req.params;
+  const { newName } = req.body;
+
+  if(users.length < index){
+    res.status(400).json({ messagem: 'Usuário não encontrado'})
+  }
+
+  users[index] = newName;
+
+  return res.json(users);
+})
+
+server.delete('/users/:index', (req, res) =>{
+
+  const { index } = req.params;
+
+  if(users.length < index){
+    res.status(400).json({ messagem: 'Usuário não encontrado'})
+  }
+
+  users.splice(index, 1);
+
+  return res.send();
+
 })
 
 server.listen(3000);
